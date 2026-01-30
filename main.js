6fe9122d4d4eeb72090a6016b2834679ddcc9dba
kiran-kharsel <kirankharsel@gmail.com>
2026-01-30 07:22:36 +0530

    feat: get all elements from html

---
+++ main.js
@@ -0,0 +1,10 @@
+// get all elements
+const prevOperandElement = document.querySelector('.prev-operand')
+const currentOperandElement = document.querySelector('.current-operand')
+
+const numberBtns = document.querySelectorAll('.operand')
+const operatorBtns = document.querySelectorAll('.operator')
+const equalBtn  = document.querySelector('.equal')
+const allClearBtn  = document.querySelector('.all-clear')
+const deleteBtn  = document.querySelector('.delete')
+
