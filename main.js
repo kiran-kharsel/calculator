a118ec4c3042a4aa62d86a0d3259014d9a8dd5ad
kiran-kharsel <kirankharsel@gmail.com>
2026-01-30 07:31:47 +0530

    feat: update display on number btn click

--- main.js
+++ main.js
@@ -8,3 +8,51 @@ const equalBtn  = document.querySelector('.equal')
 const allClearBtn  = document.querySelector('.all-clear')
 const deleteBtn  = document.querySelector('.delete')
 
+// global variable
+let currentOperand = '';
+let prevOperand = '';
+let operation;
+
+
+
+
+// all clear function
+function allClear(){}
+
+
+// delete function
+function deleteNum(){}
+
+
+// append number funtion
+function appendNum(num){
+    //check for multiple decimal point
+    if(num === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + num;
+}
+
+// chhose operation
+function chooseOperation(operation){
+
+}
+
+
+// compute
+function compute(){
+
+}
+
+
+// update display
+function updateDisplay(){
+    currentOperandElement.innerText = currentOperand;
+}
+
+
+
+numberBtns.forEach((btn) => {
+    btn.addEventListener('click', function(){
+        appendNum(btn.innerText);
+        updateDisplay()
+    })
+})
